8633a6d56055ec6867b64ef08d1852bebd03876f
VugarAhmadov <vuqar.ehmedov.94@gmail.com>
2021-01-17 14:22:18 +0400

    added swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/app/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const appConfig: AppConfigService = app.get('AppConfigService');
+  const config = new DocumentBuilder()
+    .setTitle('Lireddit API')
+    .setDescription('The Lireddit API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
+  const appConfig: AppConfigService = app.get('AppConfigService');
   await app.listen(appConfig.port, appConfig.hostname, () =>
-    console.log(
-      `App running on: ${appConfig.port}. and hostname: ${appConfig.hostname}`,
-    ),
+    Logger.log(`Server running on: ${appConfig.url}`, 'Bootstrap'),
   );
 }
 bootstrap();
